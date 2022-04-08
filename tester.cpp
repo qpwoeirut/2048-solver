96b6ff4815ae9a2bd6544939a9ee53666a30e792
qpwoeirut <stanleyzhong8@gmail.com>
2022-04-07 22:00:26 -0700

    add parallelization option

--- tester.cpp
+++ tester.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 #include <iostream>
+#include <future>
+#include <vector>
 
 #include "game/game.cpp"
 #include "solvers/ask_user.cpp"
@@ -10,22 +12,42 @@
 
 std::ofstream fout("results.csv");  // put results into a CSV
 
-const int MIN_TILE = 3;   // getting 2^3 should always be guaranteed
-const int MAX_TILE = 18;  // 2^17 is largest possible tile
+constexpr int MIN_TILE = 3;   // getting 2^3 should always be guaranteed
+constexpr int MAX_TILE = 18;  // 2^17 is largest possible tile
 
+constexpr int GAMES[5] = {100, 1000, 5000, 50000, 200000};
+constexpr int MAX_THREADS = GAMES[2];
+
+std::future<int> futures[MAX_THREADS];
 int results[MAX_TILE];
-void test_player(const std::string strategy, const int (*player)(board_t), const int games, const bool print_progress = false) {
+
+int play_game(const int (*player)(const board_t)) {
+    const board_t board = game::play(player);
+    return get_max_tile(board);
+}
+
+void test_player(const std::string strategy, const int (*player)(board_t), const int games,
+                 const bool parallelize = false, const bool print_progress = false) {
     std::cout << "\n\nTesting " << strategy << " strategy..." << std::endl;
     std::fill(results, results+MAX_TILE, 0);
 
     const long long start_time = get_current_time_ms();
-    for (int i=1; i<=games; ++i) {
-        if (print_progress && i % 5 == 0) {
-            std::cout << "Starting game #" << i << std::endl;
+    if (parallelize) {
+        assert(games <= MAX_THREADS);
+        for (int i=0; i<games; ++i) {
+            futures[i] = std::async(play_game, player);
         }
-        const board_t board = game::play(player);
-        const int max_tile = get_max_tile(board);
-        ++results[max_tile];  // suffix sum type thing
+        for (int i=0; i<games; ++i) {
+            ++results[futures[i].get()];
+        }
+    } else {
+        for (int i=1; i<=games; ++i) {
+            if (print_progress && i % 5 == 0) {
+                std::cout << "Starting game #" << i << std::endl;
+            }
+            const int max_tile = play_game(player);
+            ++results[max_tile];  // suffix sum type thing
+        }
     }
     const long long end_time = get_current_time_ms();
     for (int i=MAX_TILE-2; i>=0; --i) {
@@ -47,8 +69,6 @@ void test_player(const std::string strategy, const int (*player)(board_t), const
     fout << '\n';
 }
 
-const int GAMES[5] = {100, 1000, 5000, 50000, 500000};
-
 int main() {
     fout << "Strategy,Games,Time";
     for (int i=MIN_TILE; i<MAX_TILE; ++i) {
@@ -66,14 +86,14 @@ int main() {
     test_player("corner", corner_player::player, GAMES[4]);
     test_player("ordered", ordered_player::player, GAMES[4]);
     for (int depth=1; depth<=4; ++depth) {
-        for (int trials=1; trials<=9 - depth; ++trials) {
+        for (int trials=1; trials<=10 - depth; ++trials) {
             const std::string strategy = "merge(d=" + std::to_string(depth) + " t=" + std::to_string(trials) + ")";  // avoid printing comma since escaping annoys
             merge_player::init(depth, trials);
 
             int order = depth * 10 + trials;
-            int speed = order <= 15 ? 3 : (order <= 24 || order == 31 ? 2 : (order <= 32 ? 1 : 0));
+            int speed = order <= 15 ? 3 : (order <= 24 || order % 10 == 1 ? 2 : (order <= 33 ? 1 : 0));
 
-            test_player(strategy, merge_player::player, GAMES[speed], speed == 0);
+            test_player(strategy, merge_player::player, GAMES[speed], GAMES[speed] <= MAX_THREADS, speed == 0);
         }
     }
 
