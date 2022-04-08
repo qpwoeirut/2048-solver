60ecf83e41ce94f42087fbc679136895bfd47800
qpwoeirut <stanleyzhong8@gmail.com>
2022-04-07 23:47:43 -0700

    fix compile error and check if results dir exists

--- tester.cpp
+++ tester.cpp
@@ -25,8 +25,9 @@ const int play_game(const int (*player)(const board_t)) {
     return get_max_tile(board);
 }
 
-void save_results(const std::string& strategy, const int games, const bool parallelize) {
+void save_results(const std::string& strategy, const int games, const float time_taken, const bool parallelize) {
     std::ofstream fout("results/" + strategy + ".csv");  // put results into a one-row CSV for later collation
+    assert(fout.is_open());  // might need to create the /results directory if this doesn't work
     fout << "Strategy,Games,Time,Parallel";
     for (int i=MIN_TILE; i<MAX_TILE; ++i) {
         fout << ',' << (1 << i);
@@ -80,27 +81,23 @@ void test_player(const std::string& strategy, const int (*player)(board_t), cons
         std::cout << i << ' ' << results[i] << " (" << 100.0 * results[i] / games << ')' << std::endl;
     }
 
-    save_results(strategy, games, parallelize);
+    save_results(strategy, games, time_taken, parallelize);
 }
 
 int main() {
-
     game::init();
-
-    //game::play_slow(monte_carlo_player::player);
-
-    //game::play(user_player::player);
     
     test_player("random", random_player::player, GAMES[4]);
     test_player("corner", corner_player::player, GAMES[4]);
     test_player("ordered", ordered_player::player, GAMES[4]);
+
     for (int depth=1; depth<=4; ++depth) {
         for (int trials=1; trials<=10 - depth; ++trials) {
             const std::string strategy = "merge(d=" + std::to_string(depth) + " t=" + std::to_string(trials) + ")";  // avoid printing comma since escaping annoys
             merge_player::init(depth, trials);
 
-            int order = depth * 10 + trials;
-            int speed = order <= 15 ? 3 : (order <= 24 || order % 10 == 1 ? 2 : (order <= 33 ? 1 : 0));
+            const int order = depth * 10 + trials;
+            const int speed = order <= 15 ? 3 : (order <= 24 || order % 10 == 1 ? 2 : (order <= 33 ? 1 : 0));
 
             test_player(strategy, merge_player::player, GAMES[speed], GAMES[speed] <= MAX_THREADS, speed == 0);
         }
