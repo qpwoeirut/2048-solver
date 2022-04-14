10d0a882eed7756e44d87256703fad9cbe62e580
qpwoeirut <stanleyzhong8@gmail.com>
2022-04-13 18:18:45 -0700

    Export init and make_move from game

--- demo/selenium/export_players.cpp
+++ demo/selenium/export_players.cpp
@@ -10,10 +10,13 @@
 PYBIND11_MODULE(players, m) {
     m.doc() = "Solving strategies for 2048 written in C++ and exported to Python";
 
-    m.def("random_player", &random_player::player, "testing");
-    m.def("corner_player", &corner_player::player, "testing");
-    m.def("ordered_player", &ordered_player::player, "testing");
-    m.def("merge_player", &merge_player::player, "testing");
-    m.def("score_player", &score_player::player, "testing");
-    m.def("monte_carlo_player", &monte_carlo_player::player, "testing");
+    m.def("init_game", &game::init, "initialize game");
+    m.def("make_move", &game::make_move, "make move on board");
+
+    m.def("random_player", &random_player::player, "random player");
+    m.def("corner_player", &corner_player::player, "corner spam player");
+    m.def("ordered_player", &ordered_player::player, "ordered player");
+    m.def("merge_player", &merge_player::player, "marge player");
+    m.def("score_player", &score_player::player, "score player");
+    m.def("monte_carlo_player", &monte_carlo_player::player, "monte carlo tree search player");
 }
