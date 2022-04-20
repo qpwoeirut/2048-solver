4a763187fc4af4be6c7f113b3142d4ce58932a4f
qpwoeirut <stanleyzhong8@gmail.com>
2022-04-20 10:52:58 -0700

    Change collate.py to handle the new results format

--- results/collate.py
+++ results/collate.py
@@ -5,28 +5,31 @@
 import csv
 import os
 
-this_file = os.path.basename(__file__)
+RESULT_FILES = [
+    "random.csv", "corner.csv", "ordered.csv",
+    "merge.csv", "score.csv",
+    "minimax.csv",
+    "monte_carlo.csv"
+]
+
 this_directory = os.path.dirname(__file__)
 
 headings = None  # will be overwritten once a CSV file is read
 collated_data = []
 
-for csv_filename in sorted(os.listdir(this_directory)):  # just sort by strategy name for now
+for csv_filename in RESULT_FILES:
     filename = os.fsdecode(csv_filename)
-    if filename == this_file:  # ignore this script
-        continue
 
     with open(filename) as csv_file:
         reader = csv.reader(csv_file)
-        headings, data = reader
 
-        games = int(data[1])
-        for i in range(4, len(data)):
-            data[i] = round(int(data[i]) * 100 / games, 2)  # convert to percentage
-        collated_data.append(data)
-
-# put random, corner, ordered strategies first
-collated_data.sort(reverse=True, key=lambda row: row[0] in ["random", "corner", "ordered"])
+        rows = [r.copy() for r in reader]
+        headings, data = rows[0], rows[1:]
+        for row in data:
+            games = int(row[1])
+            for i in range(4, len(row)):
+                row[i] = round(int(row[i]) * 100 / games, 2)  # convert to percentage
+            collated_data.append(row)
 
 with open("../results.csv", 'w') as out_csv_file:
     writer = csv.writer(out_csv_file)
