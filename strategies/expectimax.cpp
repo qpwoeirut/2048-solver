e50e2159d67ecd3a577f0c8dbc7435087bbf7ca8
qpwoeirut <stanleyzhong8@gmail.com>
2022-05-16 20:20:58 -0700

    Change the evaluation for a dead board to be 15/16 of the heuristic evaluation instead of 0

--- strategies/expectimax.cpp
+++ strategies/expectimax.cpp
@@ -33,18 +33,24 @@ namespace expectimax_strategy {
         eval_t best_score = heuristics::MIN_EVAL;
         int best_move = 0;  // default best_move to 0; -1 causes issues with the packing in cases of full boards
         for (int i=0; i<4; ++i) {
+            eval_t expected_score = 0;
             const board_t new_board = game::make_move(board, i);
-            if (board == new_board || game::game_over(new_board)) continue;
-
-            eval_t expected_score = 0;  // expected score times (moves * 10)
-            const uint16_t empty_mask = to_tile_mask(new_board);
-            for (int j=0; j<16; ++j) {
-                if (((empty_mask >> j) & 1) == 0) {
-                    expected_score += 9 * (helper(new_board | (1LL << (j << 2)), cur_depth - 1, fours) >> 2);
-                    expected_score += 1 * (helper(new_board | (2LL << (j << 2)), cur_depth - 1, fours + 1) >> 2);
+            if (board == new_board) {
+                continue;
+            } else if (game::game_over(new_board)) {
+                expected_score = (MULT * evaluator(board));
+                expected_score -= expected_score >> 4;  // subtract score / 16 as penalty for dying
+            } else {
+                const uint16_t empty_mask = to_tile_mask(new_board);
+                for (int j=0; j<16; ++j) {
+                    if (((empty_mask >> j) & 1) == 0) {
+                        expected_score += 9 * (helper(new_board | (1LL << (j << 2)), cur_depth - 1, fours) >> 2);
+                        expected_score += 1 * (helper(new_board | (2LL << (j << 2)), cur_depth - 1, fours + 1) >> 2);
+                    }
                 }
+                expected_score /= count_empty(empty_mask) * 10;  // convert to actual expected score * MULT
             }
-            expected_score /= count_empty(empty_mask) * 10;  // convert to actual expected score
+
             if (best_score <= expected_score) {
                 best_score = expected_score;
                 best_move = i;
--- strategies/minimax.cpp
+++ strategies/minimax.cpp
@@ -26,15 +26,20 @@ namespace minimax_strategy {
         eval_t best_score = heuristics::MIN_EVAL;
         int best_move = 0;  // default best_move to 0; -1 causes issues with the packing in cases of full boards
         for (int i=0; i<4; ++i) {
-            const board_t new_board = game::make_move(board, i);
-            if (board == new_board || game::game_over(new_board)) continue;
-
             eval_t current_score = heuristics::MAX_EVAL;  // next step will minimize this across all tile placements
-            const uint16_t tile_mask = to_tile_mask(new_board);
-            for (int j=0; j<16; ++j) {
-                if (((tile_mask >> j) & 1) == 0) {
-                    current_score = std::min(current_score, helper(new_board | (1ULL << (j << 2)), cur_depth - 1) >> 2);
-                    current_score = std::min(current_score, helper(new_board | (2ULL << (j << 2)), cur_depth - 1) >> 2);
+            const board_t new_board = game::make_move(board, i);
+            if (board == new_board) {
+                continue;
+            } else if (game::game_over(new_board)) {
+                current_score = evaluator(board);
+                current_score -= current_score >> 4;  // subtract score / 16 as penalty for dying
+            } else {
+                const uint16_t tile_mask = to_tile_mask(new_board);
+                for (int j=0; j<16; ++j) {
+                    if (((tile_mask >> j) & 1) == 0) {
+                        current_score = std::min(current_score, helper(new_board | (1ULL << (j << 2)), cur_depth - 1) >> 2);
+                        current_score = std::min(current_score, helper(new_board | (2ULL << (j << 2)), cur_depth - 1) >> 2);
+                    }
                 }
             }
             if (best_score <= current_score) {
