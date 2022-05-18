322f13547f188a88b179bddcaa27d9303bda86d4
qpwoeirut <stanleyzhong8@gmail.com>
2022-05-18 13:03:56 -0700

    add board_sum

--- util.cpp
+++ util.cpp
@@ -67,7 +67,7 @@ int count_set(const uint16_t mask) {
 
 int count_distinct_tiles(const board_t board) {
     uint16_t tile_exists = 0;
-    for (int i=0; i<64; i+=4) if (((board >> i) & 0xF) != 0) tile_exists |= 1 << ((board >> i) & 0xF);
+    for (int i = 0; i < 64; i += 4) if (((board >> i) & 0xF) != 0) tile_exists |= 1 << ((board >> i) & 0xF);
     return count_set(tile_exists);
 }
 
@@ -75,4 +75,10 @@ int pick_depth(const board_t board) {
     const int tile_ct = count_set(to_tile_mask(board));
     const int score = count_distinct_tiles(board) + (tile_ct <= 6 ? 0 : (tile_ct - 6) >> 1);
     return score <= 7 ? 2 : (score <= 10 ? 3 : (score <= 13 ? 4 : 5 + (score >= 15)));
+}
+
+int board_sum(const board_t board) {
+    int sum = 0;
+    for (int i = 0; i < 64; i += 4) if (((board >> i) & 0xF) != 0) sum += 1 << ((board >> i) & 0xF);
+    return sum;
 }
