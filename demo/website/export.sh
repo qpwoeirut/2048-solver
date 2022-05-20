fa1898babea926f523e4378c5d7d32eb979eb31f
qpwoeirut <stanleyzhong8@gmail.com>
2022-05-20 10:09:20 -0700

    Get rid of USE_CACHE flag, always use cache

--- demo/website/export.sh
+++ demo/website/export.sh
@@ -1,4 +1,4 @@
-em++ --std=c++20 -O3 -Wall -funroll-loops export_players.cpp -o players.js -DUSE_CACHE \
+em++ --std=c++20 -O3 -Wall -funroll-loops export_players.cpp -o players.js \
 -I /usr/local/Cellar/google-sparsehash/2.0.4/include/ \
 --no-entry -sWASM_BIGINT -sALLOW_MEMORY_GROWTH -sENVIRONMENT=web -sFILESYSTEM=0 -flto \
 -sEXPORTED_FUNCTIONS=_init_game,_random_player,_spam_corner_player,_ordered_player,_rotating_player,_rand_trials_player,_init_rand_trials_strategy,_minimax_player,_init_minimax_strategy,_expectimax_player,_init_expectimax_strategy,_monte_carlo_player,_init_monte_carlo_strategy
--- strategies/expectimax.cpp
+++ strategies/expectimax.cpp
@@ -8,7 +8,6 @@ namespace expectimax_strategy {
     int depth = 0;
     heuristic_t evaluator = heuristics::dummy_heuristic;
 
-    #ifdef USE_CACHE
     constexpr int CACHE_DEPTH = 2;
     cache_t cache(1 << 20);
     bool cache_empty_key_set = false;
@@ -17,11 +16,6 @@ namespace expectimax_strategy {
     // speed things up with integer arithmetic
     // expected score * 10, 4 moves, 30 tile placements, multiplied by 4 to pack score and move, times 16 to pack cache
     constexpr eval_t MULT = 4e18 / (heuristics::MAX_EVAL * 10 * 4 * 30 * 4 * 16);
-    #else
-    // speed things up with integer arithmetic
-    // expected score * 10, 4 moves, 30 tile placements, multiplied by 4 to pack score and move
-    constexpr eval_t MULT = 4e18 / (heuristics::MAX_EVAL * 10 * 4 * 30 * 4);
-    #endif
     static_assert(MULT > 1);
 
     const eval_t helper(const board_t board, const int cur_depth, const bool add_to_cache, const int fours) {
@@ -33,7 +27,6 @@ namespace expectimax_strategy {
             return (MULT * evaluator(board)) << 2;  // move doesn't matter
         }
 
-        #ifdef USE_CACHE
         if (cur_depth >= CACHE_DEPTH) {
             const cache_t::iterator it = cache.find(board);
             #ifdef REQUIRE_DETERMINISTIC
@@ -42,7 +35,6 @@ namespace expectimax_strategy {
             if (it != cache.end() && (it->second & 0xF) >= cur_depth) return it->second >> 4;
             #endif
         }
-        #endif
 
         eval_t best_score = heuristics::MIN_EVAL;
         int best_move = 0;  // default best_move to 0; -1 causes issues with the packing in cases of full boards
@@ -68,11 +60,9 @@ namespace expectimax_strategy {
             }
         }
 
-        #ifdef USE_CACHE
         if (add_to_cache && cur_depth >= CACHE_DEPTH) {
             cache[board] = (((best_score << 2) | best_move) << 4) | cur_depth;
         }
-        #endif
 
         return (best_score << 2) | best_move;  // pack both score and move
     }
@@ -85,15 +75,12 @@ namespace expectimax_strategy {
 
     const int player(const board_t board) {
         const int depth_to_use = depth <= 0 ? pick_depth(board) - depth : depth;
-        #ifdef USE_CACHE
+
         // if depth <= CACHE_DEPTH + 1, caching results isn't worth it
         const bool add_to_cache = true;  //depth_to_use > CACHE_DEPTH + 1;
         cache.clear_no_resize();
         const int move = helper(board, depth_to_use, add_to_cache, 0) & 3;
         std::cout << cache.size() << '\n';
-        #else
-        const int move = helper(board, depth_to_use, false, 0) & 3;
-        #endif
         return move;
     }
 
@@ -101,7 +88,6 @@ namespace expectimax_strategy {
         depth = _depth;
         evaluator = _evaluator;
 
-        #ifdef USE_CACHE
         if (!cache_empty_key_set) {  // empty_key hasn't been set, so let's set everything
             cache_empty_key_set = true;
             cache.set_empty_key(game::INVALID_BOARD);
@@ -109,7 +95,6 @@ namespace expectimax_strategy {
             cache.min_load_factor(0.0);
         }
         cache.clear_no_resize();
-        #endif
     }
 }
 
