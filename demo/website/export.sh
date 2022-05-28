9916a89ad994aed6a8d6c28a75c69d6541d7bc52
qpwoeirut <stanleyzhong8@gmail.com>
2022-05-27 21:09:28 -0700

    Reinstall sparsehash into /usr/local/include

--- demo/website/export.sh
+++ demo/website/export.sh
@@ -1,4 +1,4 @@
 em++ --std=c++20 -O3 -Wall -funroll-loops -fconstexpr-steps=0x500000 export_players.cpp -o players.js \
--I /usr/local/Cellar/google-sparsehash/2.0.4/include/ \
 --no-entry -lembind -sWASM_BIGINT -sALLOW_MEMORY_GROWTH -sENVIRONMENT=web -sFILESYSTEM=0 -flto \
+-I /usr/local/include
 #-g -sASSERTIONS=2 -sDEMANGLE_SUPPORT=1 -sSAFE_HEAP=1 -sSTACK_OVERFLOW_CHECK=2
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 ext = Pybind11Extension("players", ["demo/selenium/export_players.cpp"])
 ext.cxx_std = 20
 # surely there must be a better way to do this?
-ext._add_cflags(["-funroll-loops", "-fconstexpr-steps=0x500000", "-I/usr/local/Cellar/google-sparsehash/2.0.4/include/"])
+ext._add_cflags(["-funroll-loops", "-fconstexpr-steps=0x500000", "-I/usr/local/include"])
 
 ext_modules = [
     ext
